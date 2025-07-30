67b50d20e467443191300bea6d3569a5d71c455f
Félix Antonio Pérez Abreu <fperez@blackhole.localdomain>
2025-07-29 21:56:58 -0400

    Instala los Plugins zsh-autosuggestions y zsh-syntax-highlighting

--- setup_linux.sh
+++ setup_linux.sh
@@ -10,4 +10,12 @@ if [[ "$SHELL" != *"zsh"* ]]; then
     echo "Shell cambiada a Zsh. Por favor, reinicia la terminal."
 fi
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Instalar zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+
+# Instalar zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 echo "Configuración de Linux completada."
