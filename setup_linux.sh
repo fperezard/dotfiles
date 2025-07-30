13b7e700600a6996f60c9b209b5523a98be6fac5
Félix Antonio Pérez Abreu <fperez@blackhole.localdomain>
2025-07-29 21:49:19 -0400

    instalando oh my zsh

--- setup_linux.sh
+++ setup_linux.sh
@@ -2,11 +2,12 @@
 # SCRIPT DE SETUP PARA LINUX (NATIVO O WSL)
 echo "Iniciando configuración del entorno Linux..."
 sudo apt-get update
-sudo apt-get install -y zsh tmux neovim stow git build-essential
+sudo apt-get install -y zsh tmux neovim stow git ripgrep fzf build-essential 
 stow -d configs -t ~ zsh
 stow -d configs -t ~ tmux
 if [[ "$SHELL" != *"zsh"* ]]; then
     chsh -s $(which zsh)
     echo "Shell cambiada a Zsh. Por favor, reinicia la terminal."
 fi
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo "Configuración de Linux completada."
